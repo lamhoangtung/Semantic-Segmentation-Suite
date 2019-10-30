9486e5a1b8d76fb7269cfd5b83c37739e9eeacf6
lamhoangtung <lamhoangtung.vz@gmail.com>
2019-10-30 23:31:51 +0700

    Add setup script

---
+++ scripts/setup.sh
@@ -0,0 +1,22 @@
+# cd ~
+# wget https://github.com/lamhoangtung/kaggle-kernel-setup/raw/master/install_common.sh
+# chmod 777 install_common.sh
+# ./install_common.sh
+# source .bashrc
+# tmux
+
+echo "Clone the code"
+git clone https://github.com/lamhoangtung/Semantic-Segmentation-Suite
+cd Semantic-Segmentation-Suite
+git checkout tiny_cityscape
+echo "Install dependencies"
+python3 -m pip install -r requirements.txt
+echo "Download and preprecess the training data"
+gdrive download 1Y8rolmOGQ4E7Z4fDXJ5UZODD7jhBCN8K
+unzip -qq city_scape.zip
+chmod 777 ./scripts/*.sh
+# For logging
+apt-get -y install expect
+conda install -c eumetsat expect
+# trains-init
+echo "Done. Run ./scripts/train_mobile-net-v2_mobile-unet-skip.sh to start training!"
