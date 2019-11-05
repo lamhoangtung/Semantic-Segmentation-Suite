8f13c1569eb3b1aaa839384af67f682672c77310
lamhoangtung <lamhoangtung.vz@gmail.com>
2019-11-05 22:41:49 +0700

    ready for more

--- scripts/train_mobile-net-v2_dense-aspp.sh
+++ scripts/train_mobile-net-v2_dense-aspp.sh
@@ -1,17 +1,17 @@
 python3 train.py --num_epochs 600 \
                   --checkpoint_step 1 \
                   --validation_step 1 \
-                  --dataset city_scape \
+                  --dataset apolo_city_scape \
                   --crop_height 224 \
                   --crop_width 320 \
-                  --batch_size 16 \
-                  --num_val_images 32 \
+                  --batch_size 64 \
+                  --num_val_images 64 \
                   --h_flip true \
                   --v_flip true \
                   --brightness 0.1 \
                   --rotation 5 \
                   --model DenseASPP \
                   --frontend MobileNetV2 \
-                  --train_dir tiny-cityscape_mobile-net-v2_dense-aspp
+                  --train_dir apolo-cityscape_mobile-net-v2_dense-aspp
                 #   --epoch_start_i 322 \
                 #   --continue_training true
