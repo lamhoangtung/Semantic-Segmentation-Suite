2b5ab3f094368e2f68c75d3e6c1b0e763c87931f
lamhoangtung <lamhoangtung.vz@gmail.com>
2019-11-10 17:47:29 +0700

    meh

--- scripts/train_mobile-net-v2_dense-aspp.sh
+++ scripts/train_mobile-net-v2_dense-aspp.sh
@@ -4,8 +4,8 @@ python3 train.py --num_epochs 100 \
                   --dataset apolo_city_scape \
                   --crop_height 224 \
                   --crop_width 320 \
-                  --batch_size 32 \
-                  --num_val_images 64 \
+                  --batch_size 64 \
+                  --num_val_images 128 \
                   --h_flip true \
                   --v_flip true \
                   --brightness 0.1 \
