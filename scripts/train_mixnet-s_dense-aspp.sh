4bf4684384ab36fd8a43b1ccb838b355c1063762
lamhoangtung <lamhoangtung.vz@gmail.com>
2019-11-11 23:52:17 +0700

    Add MixNet training script

---
+++ scripts/train_mixnet-s_dense-aspp.sh
@@ -0,0 +1,22 @@
+python3 train.py --num_epochs 500 \
+                  --checkpoint_step 1 \
+                  --validation_step 1 \
+                  --dataset apolo_city_scape \
+                  --crop_height 224 \
+                  --crop_width 320 \
+                  --batch_size 32 \
+                  --num_val_images 128 \
+                  --h_flip true \
+                  --v_flip true \
+                  --brightness 0.1 \
+                  --rotation 5 \
+                  --model DenseASPP \
+                  --frontend MixNet_S \
+                  --loss self_balanced_focal_loss \
+                  --lr_warmup false \
+                  --learning_rate 0.001 \
+                  --optimizer adam \
+                  --train_dir apolo-cityscape_mixnet-s_dense-aspp_self-bl-focal_adam
+                  # --lr_scheduler poly_decay \
+                #   --epoch_start_i 322 \
+                #   --continue_training true
