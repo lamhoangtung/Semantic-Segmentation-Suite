ab1cade69fd776a5f0bbdf79f53d66d4f74654c6
lamhoangtung <lamhoangtung.vz@gmail.com>
2019-11-15 10:58:25 +0700

    nah

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -12,7 +12,7 @@ git checkout tiny_cityscape
 echo "Install dependencies"
 # python3 -m pip install -r requirements.txt
 echo "Download and preprecess the training data"
-gdrive download 1UB81Wx44nYOBLzQckwrKAu3wkgnisqW9
+gdrive download 1EIsMgxhsUXQd2fJcBzfeRgdezlSM27cl
 unzip -qq apolo_city_scape_no_vehicle.zip
 chmod 777 ./scripts/*.sh
 echo "Done. Run ./scripts/train_mobile-net-v2_mobile-unet-skip.sh to start training!"
